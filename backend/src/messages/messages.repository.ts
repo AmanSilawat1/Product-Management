e2e1f528dac239f8e6378ee085d65e9c3303c53f
AmanSilawat1 <aman.silawat@napses.com>
2026-01-07 17:27:10 +0530

    create, get single, get all messages using services and repository with DTO

---
+++ backend/src/messages/messages.repository.ts
@@ -0,0 +1,28 @@
+import { readFile, writeFile } from 'fs/promises';
+
+
+
+export class MessagesRepository {
+
+    async findOne(id: string) {
+        const content = await readFile('messages.json', 'utf8');
+        const messages = JSON.parse(content);
+        return messages[id];
+    }
+
+    async findAll() {
+        const content = await readFile('messages.json', 'utf8');
+        const messages = JSON.parse(content)
+        return messages;
+    }
+
+    async createMessage(content: string) {
+        const contents = await readFile('messages.json', 'utf8');
+        const messages = JSON.parse(contents);
+
+        const id = Math.floor(Math.random() * 1000);
+        messages[id] = { id, content }
+
+        await writeFile('messages.json', JSON.stringify(messages));
+    }
+}
